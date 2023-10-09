1d8f2b7cf4ef67663df76e626fd1c424dd38cde5
Brett <me.caramackid@gmail.com>
2023-10-09 17:05:58 +0100

    Changed API, added options and error checking

--- main.go
+++ main.go
@@ -1,14 +1,76 @@
 package main
 
 import (
-    "net/http"
-    "io"
-    "fmt"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
+type IPType struct {
+	QUERY  string
+	ISP    string
+	CITY   string
+	ZIP    string
+	REGION string
+	CC     string `json:"CountryCode"`
+}
+
+type DNSType struct {
+	DNS struct {
+		GEO string
+		IP  string
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
-    res, _ := http.Get("https://ifconfig.me/ip")
-    defer res.Body.Close()
-    body, _ := io.ReadAll(res.Body)
-    fmt.Println(string(body))
+	getLocation := flag.Bool("location", false, "Get location data")
+	getDNS := flag.Bool("dns", false, "Get DNS data")
+	getISP := flag.Bool("isp", false, "Get ISP data")
+	flag.Parse()
+
+	var IPData IPType
+	var DNSData DNSType
+
+	ip := flag.Args()
+	res, err := http.Get(fmt.Sprintf("http://ip-api.com/json/%s?fields=8758", ip[0]))
+	check(err)
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	check(err)
+	err = json.Unmarshal(body, &IPData)
+	check(err)
+
+	DNSRes, err := http.Get("http://edns.ip-api.com/json/")
+	check(err)
+	defer DNSRes.Body.Close()
+	DNSBody, err := io.ReadAll(DNSRes.Body)
+	check(err)
+	err = json.Unmarshal(DNSBody, &DNSData)
+	check(err)
+
+	IP := IPData.QUERY
+	Location := fmt.Sprintf("%s [%s] | %s | %s", IPData.CITY, IPData.ZIP, IPData.REGION, IPData.CC)
+	ISP := IPData.ISP
+	DNS := fmt.Sprintf("%s [%s]", DNSData.DNS.GEO, DNSData.DNS.IP)
+
+	fmt.Println(IP)
+	if *getLocation {
+		fmt.Println(Location)
+	}
+	if *getISP {
+		fmt.Println(ISP)
+	}
+	if *getDNS {
+		fmt.Println(DNS)
+	}
 }
